37e25073c5c7b4dca0bb8634b822245373cf8088
Ionescu Florin-Eugen <ionescuflorin-eugen@MacBook-Pro-Ionescu.local>
2020-09-17 09:06:17 +0300

    propagating_the_event_this_not_bound

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,9 @@
 class ProductList extends React.Component {
+  // 1. creating the function with desired logic
+  handleProductUpVote(productId) {
+    console.log(productId + ' was upvoted.');
+  }
+
   render() {
     // Source of data
     const products = Seed.products.sort((a, b) => b.votes - a.votes);
@@ -12,6 +17,8 @@ class ProductList extends React.Component {
         votes={product.votes}
         submitterAvatarUrl={product.submitterAvatarUrl}
         productImageUrl={product.productImageUrl}
+        // 2. passing down the function as prop to be sended to the child
+        onVote={this.handleProductUpVote}
       />
     ));
     return <div className="ui unstackable items">{productComponents}</div>;
@@ -19,6 +26,16 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
+  
+  // 3. create a method that takes parent props
+  /**
+   * Here’s the odd part: When working inside render(), we’ve witnessed that this is always bound to the component. 
+   * But inside our custom component method handleUpVote(), this is actually null.
+   */
+  handleUpVote() {
+    this.props.onVote(this.props.id);
+  }
+
   render() {
     return (
       <div className="item">
@@ -27,8 +44,9 @@ class Product extends React.Component {
         </div>
         <div className="middle aligned content">
           <div className="header">
-            {' '}
-            <a>
+            {/* 4. executing that function when the desired interaction occurs */}
+            {/* we'll have an eror: cannot read property of props of undefined due to not binding the method */}
+            <a onClick={this.handleUpVote}>
               <i className="large caret up icon" />{' '}
             </a>
             {this.props.votes}{' '}
