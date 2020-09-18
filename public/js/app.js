fda8728673bc8bee74234ae85c0c676c3e6627f9
Ionescu Florin-Eugen <ionescuflorin-eugen@MacBook-Pro-Ionescu.local>
2020-09-18 15:54:03 +0300

    updating_state_and_immutability

--- public/js/app.js
+++ public/js/app.js
@@ -2,15 +2,27 @@ class ProductList extends React.Component {
   constructor(props) {
     super(props);
     this.state = { products: [] };
+
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
   }
 
-  // after mounting the with an empty state and then populate with data from Seed
   componentDidMount() {
     this.setState({ products: Seed.products });
   }
 
   handleProductUpVote(productId) {
-    console.log(productId + ' was upvoted.');
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({
+      products: nextProducts,
+    });
   }
 
   render() {
